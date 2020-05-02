d809ab896e3b4a9057de58bff5cb02ce5af5d37e
Nolan Trapp <officialstackemup@gmail.com>
2020-05-02 16:49:48 -0700

    0.0.1

---
+++ src/com/magicaldreams/Magicband/Main.java
@@ -0,0 +1,21 @@
+package com.magicaldreams.Magicband;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+		System.out.println("MagicBand Enabled");
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		System.out.println("MagicBand Disabled");
+		
+	}
+
+}
