83488f7fc774fc9e7023b2776eeb7987345a1d48
Nolan Trapp <officialstackemup@gmail.com>
2020-05-03 11:09:04 -0700

    0.1.1

--- src/com/magicaldreams/Magicband/Main.java
+++ src/com/magicaldreams/Magicband/Main.java
@@ -1,45 +1,51 @@
 package com.magicaldreams.Magicband;
 
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
+
+
+
 public class Main extends JavaPlugin{
+	public final Logger logger = this.getLogger();
+	
+
 	
 	@Override
 	public void onEnable() {
 		
 		System.out.println("MagicBand Enabled");
 		
-        Bukkit.getPluginManager().registerEvents(new OpenBand(this), this);
-
 		
-		
+		Bukkit.getPluginManager().registerEvents(new OpenBand(this), this);
+				
 	}
 	
 	public void applyMagicBandUI(Player player) {
         //Beginning
-        Inventory MagicbandGUI = Bukkit.createInventory(null, 9, ChatColor.BLUE + "MagicBand");
+        Inventory MagicbandGUI = Bukkit.createInventory(null, 27, ChatColor.BLUE + "MagicBand");
 	
         
-        ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
-
-        
-        MagicbandGUI.setItem(0, LEATHER_HELMET);
         
         
         player.openInventory(MagicbandGUI);
 	}
 	
+
 	
-	
+
 	
 	@Override
 	public void onDisable() {
--- src/com/magicaldreams/Magicband/OpenBand.java
+++ src/com/magicaldreams/Magicband/OpenBand.java
@@ -1,20 +1,12 @@
 package com.magicaldreams.Magicband;
 
-import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
 
-import net.minecraft.server.v1_14_R1.ItemStack;
 
 public class OpenBand implements Listener{
 	
@@ -25,20 +17,25 @@ public class OpenBand implements Listener{
 		
 		this.main = main;
 	}
-
-	@EventHandler
-	public void onInventoryClick(InventoryClickEvent e){
-		
-	if(e.getCurrentItem().getType() == Material.FIREWORK_STAR) {
-		
-		
-		
-		main.applyMagicBandUI((Player) e);
-		((Player) e).playSound(((OfflinePlayer) e).getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 5.0F, 1.0F);
-	}
-	}
-
-
 	
+	
+	
+	
+	@EventHandler
+	public void PlayerMB(PlayerInteractEvent e){
+		
+		Player player = e.getPlayer();
+		
+	
+		
+		System.out.println("Opened");
+
+		
+		main.applyMagicBandUI((Player) player);
+	
+	
+	}
+
+
 
 }
