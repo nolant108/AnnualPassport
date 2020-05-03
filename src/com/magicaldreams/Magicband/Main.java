ffae1bd69cb51685be818a0b67239932763de809
Nolan Trapp <officialstackemup@gmail.com>
2020-05-03 13:01:31 -0700

    0.2.0

--- src/com/magicaldreams/Magicband/Main.java
+++ src/com/magicaldreams/Magicband/Main.java
@@ -1,18 +1,21 @@
 package com.magicaldreams.Magicband;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.Sound;
+import org.bukkit.SkullType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.ItemStack;
+
+import net.minecraft.server.v1_14_R1.Item;
 
 
 
@@ -36,8 +39,17 @@ public class Main extends JavaPlugin{
 	public void applyMagicBandUI(Player player) {
         //Beginning
         Inventory MagicbandGUI = Bukkit.createInventory(null, 27, ChatColor.BLUE + "MagicBand");
-	
+	        
         
+        ItemStack skull = new ItemStack(Material.LEGACY_SKULL_ITEM, 1, (short) 3);
+        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        skullMeta.setOwner(player.getName());
+        skullMeta.setDisplayName(ChatColor.YELLOW + player.getName() + "'s MagicBand");
+        skull.setItemMeta(skullMeta);
+        
+        
+        MagicbandGUI.setItem(4, skull);
+
         
         
         player.openInventory(MagicbandGUI);
--- src/com/magicaldreams/Magicband/OpenBand.java
+++ src/com/magicaldreams/Magicband/OpenBand.java
@@ -1,17 +1,24 @@
 package com.magicaldreams.Magicband;
 
+import java.util.ArrayList;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 
 public class OpenBand implements Listener{
@@ -49,12 +56,45 @@ public class OpenBand implements Listener{
 		
 		Player player = e.getPlayer();
 		
-	
 		
+	
+        ((Player) player).playSound(((OfflinePlayer) player).getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 2.0F, 1.0F);
 		
 		main.applyMagicBandUI((Player) player);
+		
+		
+		
+		
+		
 	
+	}
+
+
+	@EventHandler
+	public void onClick(InventoryClickEvent e) {
+		
+        Player player = (Player) e.getWhoClicked();
+		
+		
+		if( e.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + "MagicBand")) {
+
+            if(e.getCurrentItem() != null) {
+                e.setCancelled(true);
+                switch (e.getCurrentItem().getType()) {
+                
+                case LEGACY_SKULL_ITEM:
+                    player.closeInventory();
+
+                	
+                    break;
+                default:
+                    return;
 	
+                }
+            }
+	 }
+
+		
 	}
 
 
