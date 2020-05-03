4ff2a8fbab1e3f1bebfda8ea2cdde711517d61f3
Nolan Trapp <officialstackemup@gmail.com>
2020-05-03 11:18:53 -0700

    0.1.1

--- src/com/magicaldreams/Magicband/OpenBand.java
+++ src/com/magicaldreams/Magicband/OpenBand.java
@@ -1,11 +1,17 @@
 package com.magicaldreams.Magicband;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 
 public class OpenBand implements Listener{
@@ -19,6 +25,23 @@ public class OpenBand implements Listener{
 	}
 	
 	
+	ItemStack JoinMB;
+    ItemMeta JoinMBMeta;
+	@EventHandler
+	public void MBJoin(PlayerJoinEvent e){
+		
+		Player player = e.getPlayer();
+		
+		JoinMB = new ItemStack(Material.FIREWORK_STAR);
+
+		JoinMBMeta = JoinMB.getItemMeta();
+		JoinMBMeta.setDisplayName(ChatColor.BLUE + "MagicBand");
+		
+		player.getInventory().setItem(0, JoinMB);
+		
+	}
+	
+	
 	
 	
 	@EventHandler
@@ -28,8 +51,6 @@ public class OpenBand implements Listener{
 		
 	
 		
-		System.out.println("Opened");
-
 		
 		main.applyMagicBandUI((Player) player);
 	
